dd80d462508fa4442efb8bd1c451c211f9eda8c3
Jason Felice <jason.m.felice@gmail.com>
2019-10-17 17:42:16 -0400

    Correct style of derivation for tmux plugin

--- derivation.nix
+++ derivation.nix
@@ -1,16 +1,6 @@
-{ stdenv, fetchFromGitHub, ... }:
+{ tmuxPlugins, fetchFromGitHub, ... }:
 
-stdenv.mkDerivation rec {
-  pname = "tmux-plumb";
-  version = "0.1.0";
-
+tmuxPlugins.mkDerivation {
+  pluginName = "plumb";
   src = ./.;
-
-  meta = with stdenv.lib; {
-    description = "TODO: fill me in";
-    homepage = https://github.com/eraserhd/tmux-plumb;
-    license = licenses.publicDomain;
-    platforms = platforms.all;
-    maintainers = [ maintainers.eraserhd ];
-  };
 }
