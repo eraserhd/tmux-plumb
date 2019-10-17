3903cb4ce6391352dd8e606b3a95215d1c96467f
Jason Felice <jason.m.felice@gmail.com>
2019-10-17 17:53:51 -0400

    Use 9 from plan9port

--- derivation.nix
+++ derivation.nix
@@ -1,6 +1,9 @@
-{ tmuxPlugins, fetchFromGitHub, ... }:
+{ pkgs, tmuxPlugins, fetchFromGitHub, ... }:
 
 tmuxPlugins.mkDerivation {
   pluginName = "plumb";
+  postInstall = ''
+    sed -i -e 's,9 plumb,${pkgs.plan9port}/bin/9 plumb,' $target/scripts/plumb
+  '';
   src = ./.;
 }
