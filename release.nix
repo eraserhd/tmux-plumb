de242c956772e32f0409f2cc7d0f8b261fbc5e58
Jason Felice <jason.m.felice@gmail.com>
2020-03-29 14:40:05 -0400

    Use overlays in release.nix

--- release.nix
+++ release.nix
@@ -1,10 +1,14 @@
 { nixpkgs ? (import ./nixpkgs.nix), ... }:
 let
-  pkgs = import nixpkgs { config = {}; };
-  tmux-plumb = pkgs.callPackage ./derivation.nix {};
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [
+      (import ./overlay.nix)
+    ];
+  };
 in {
   test = pkgs.runCommandNoCC "tmux-plumb-test" {} ''
     mkdir -p $out
-    : ${tmux-plumb}
+    : ${pkgs.tmuxPlugins.plumb}
   '';
 }
